91bccda5206c1693d13a9ee14c2062d6fa755ede
shalabycr7 <cr7.shalaby@gmail.com>
2020-09-12 18:33:33 +0200

    Add active state while scroll among sections

--- js/app.js
+++ js/app.js
@@ -67,4 +67,21 @@ window.onscroll = ()=> {
          scrollTo({top:0,left:0,behavior:'smooth'});
 })
 }
+var allSections=document.querySelectorAll('section');
+for (var element of allSections){
+   var position = element.getBoundingClientRect();
+   // checking whether fully visible 
+if(position.top >= 0 && position.bottom <= window.innerHeight) {
+  /* console.log('Element is fully visible in screen'); */
+for (var i = 0; i < sections.length; i++) {
+      sections[i].classList.remove("your-active-class");
+    }
+element.classList.add('your-active-class');
+} 	
+// checking for partial visibility 
+/*if(position.top < window.innerHeight && position.bottom >= 0) {
+   console.log('Element is partially visible in screen');
+}*/
 }
+}
+
